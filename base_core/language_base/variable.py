463ea428d492326a39871a76eca8742dd0f7ed5e
Kong-De-Yi <kdeyi@163.com>
2025-12-10 17:48:02 +0800

    update

--- base_core/language_base/variable.py
+++ base_core/language_base/variable.py
@@ -1,3 +1,7 @@
 d1 = {"name": "Alice", "age": 30, "city": "New York"}
 d2 = dict(name="Alice", age=30, city="New York")
 d3 = dict([("name", "Alice"), ("age", 30), ("city", "New York")])
+
+print(d1["name"])
+print(d2.get("name"))
+print(d3.get("email", "Not Found"))
