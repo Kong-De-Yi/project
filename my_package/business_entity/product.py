6893ffdb2a7dc84981d7b78e7a1a87633252adc9
Kong-De-Yi <kdeyi@163.com>
2025-12-10 11:26:04 +0800

    update

---
+++ my_package/business_entity/product.py
@@ -0,0 +1,4 @@
+class StyleDesign:
+    """款式设计"""
+    def __init__(self,third-level_category,launch_year,launch_season,applicable_gender,fourth-level_category,design_style,design_draft):
+            pass
