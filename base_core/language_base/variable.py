5a5af2eeb8f86800d0ac13ca8c69dcca64a3b5c0
Kong-De-Yi <kdeyi@163.com>
2025-12-10 16:05:50 +0800

    update

--- base_core/language_base/variable.py
+++ base_core/language_base/variable.py
@@ -1,1 +1,14 @@
 name = "Jim"
+first_name = "Halpert"
+last_name = "Halpert"
+full_name = first_name + " " + last_name
+greeting = "Hello, " + name + "!"
+age = 30
+height_cm = 183.0
+is_employee = True
+skills = ["Sales", "Management", "Customer Service"]
+address = {"street": "123 Paper St.", "city": "Scranton", "state": "PA", "zip": "18503"}
+salary = None  # Salary is not yet defined
+performance_score = 4.5  # Out of 5.0
+work_hours = (9, 17)  # From 9 AM to 5 PM
+project_codes = {"PRJ001", "PRJ002", "PRJ003"}
