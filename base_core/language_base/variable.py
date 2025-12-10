23c7bad6be96461a6afd2c4b3ee110ab41f7afec
Kong-De-Yi <kdeyi@163.com>
2025-12-10 18:10:51 +0800

    update

--- base_core/language_base/variable.py
+++ base_core/language_base/variable.py
@@ -1,7 +1,14 @@
-d1 = {"name": "Alice", "age": 30, "city": "New York"}
-d2 = dict(name="Alice", age=30, city="New York")
-d3 = dict([("name", "Alice"), ("age", 30), ("city", "New York")])
+# 映射
+"""创建"""
+d1 = {"name": "Alice", "age": 25, "city": "Beijing"}
+d2 = dict(name="Alice", age=25, city="Beijing ")
+d3 = dict([("name", "Alice"), ("age", 25), ("city", "Beiing")])
+
+"""使用字典推导式"""
+squares = {x: x**2 for x in range(5)}
+print(squares)
 
 print(d1["name"])
-print(d2.get("name"))
-print(d3.get("email", "Not Found"))
+print(d1.get("age"))
+print(d1.get("city", "Not Found"))
+print(d1.get("email", "Not Found"))
