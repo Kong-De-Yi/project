61f487ec30cd50b9bc24419033bebe16e5daa551
Kong-De-Yi <kdeyi@163.com>
2025-12-10 11:35:55 +0800

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+from my_package.business_entity.product import StyleDesign
+
+obj = StyleDesign(
+    third_level_category="上衣",
+    launch_year=2023,
+    launch_season="春季",
+    applicable_gender="女",
+    fourth_level_category="衬衫",
+    design_style="休闲",
+    design_draft="设计稿文件路径或描述",
+)
