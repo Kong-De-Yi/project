069b426c5e1797121673f418678b8efa1cd2c6c6
Kong-De-Yi <kdeyi@163.com>
2025-12-11 21:20:30 +0800

    update

--- base_core/language_base/variable.py
+++ base_core/language_base/variable.py
@@ -1,12 +1,6 @@
-# 序列
-
-
-# 映射
-"""创建"""
-d1 = {"name": "Alice", "age": 25, "city": "Beijing"}
-d2 = dict(name="Alice", age=25, city="Beijing ")
-d3 = dict([("name", "Alice"), ("age", 25), ("city", "Beiing")])
-
-"""使用字典推导式"""
-squares = {x: x**2 for x in range(5)}
-print(squares)
+try:
+    with open("D:\\Hello.txt") as f:
+        for line in f:
+            print(line, end="")
+except Exception as e:
+    print(e)
